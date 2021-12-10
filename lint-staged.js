9e90d35f51b697f4e0f3b5028163df2011075b45
Amren Stephenson <92263829+amrenstephenson@users.noreply.github.com>
2021-12-10 12:47:45 +0000

    Update hook

--- lint-staged.js
+++ lint-staged.js
@@ -1,8 +1,6 @@
 // From https://blog.logrocket.com/build-robust-react-app-husky-pre-commit-hooks-github-actions/ [accessed 10 Dec 2021]
 module.exports = {
-    '*.{js,jsx,ts,tsx}': [
-        'eslint --max-warnings=0',
-        'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-        () => 'tsc-files --noEmit'
+    '*.js': [
+        'npm test'
     ]
 };
