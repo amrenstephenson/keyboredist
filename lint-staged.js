1ca02de686ec1152be62670854c90d1fa33d13fc
Amren Stephenson <92263829+amrenstephenson@users.noreply.github.com>
2021-12-10 13:04:47 +0000

    Tabs <3

--- lint-staged.js
+++ lint-staged.js
@@ -1,6 +1,6 @@
 // From https://blog.logrocket.com/build-robust-react-app-husky-pre-commit-hooks-github-actions/ [accessed 10 Dec 2021]
 module.exports = {
-    '*.js': [
-        'npm test'
-    ]
+	'*.js': [
+		'npm test'
+	]
 };
